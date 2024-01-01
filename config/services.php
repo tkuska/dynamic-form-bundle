571467cfdc6d3cdbb0d206f94a57d7d320b152de
Tomasz Kuśka <tomasz.kuska@softiq.pl>
2024-01-01 15:42:21 +0100

    Add config/services.php file
    This commit introduces the config/services.php file to the codebase. The file is set up with a namespace for Symfony's DependencyInjection and defines a basic return function for a container configurator.

---
+++ config/services.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+    $services = $container->services();
+};
